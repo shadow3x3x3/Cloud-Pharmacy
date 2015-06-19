49a1ae66456e8dade18f5661a5bc4030b8aae8ae
sheng wei qiu <shadow3x3x3@gmail.com>
2015-06-19 17:33:33 +0800

    fixing chinese in PDF (need rtf)

--- app/pdfs/af_pdf.rb
+++ app/pdfs/af_pdf.rb
@@ -3,11 +3,12 @@ class AfPdf < Prawn::Document
     super()
     @assessmentForm = assessmentForm
     @view = view
-    text "hell world"
-
+    # background_image
+    text "中文可以嗎"
   end
 
   def background_image
+    image_path = "#{Rails.root}/app/assets/images/post.png"
 
   end
 end
