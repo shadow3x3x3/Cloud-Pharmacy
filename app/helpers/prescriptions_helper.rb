e39096fecf77848a07120534cee7d8bdc46ede3d
sheng wei qiu <shadow3x3x3@gmail.com>
2016-01-11 17:54:15 +0800

    ruby code style in prescriptions helper

--- app/helpers/prescriptions_helper.rb
+++ app/helpers/prescriptions_helper.rb
@@ -3,14 +3,14 @@ module PrescriptionsHelper
   # 辨別是散客還是住民
   # @param  prescription
   # @return "散客" or "住民"
-  def identityCheck prescription
+  def identityCheck(prescription)
     PrescriptionOfAll.find(prescription.prescriptionID).identityCheck == "fit" ? "散客" : "住民"
   end
 
   # 目前身分的姓名
   # @param  prescription
   # @return 散客姓名 or 住民姓名
-  def identityCheckName prescription
+  def identityCheckName(prescription)
     if identityCheck(prescription) == "散客"
       Fit.find(prescription.prescriptionID).name
     else
@@ -21,14 +21,14 @@ module PrescriptionsHelper
   # 處方箋電聯狀態
   # @param  prescription
   # @return 有電聯 or 未電聯
-  def phoneStatus prescription
+  def phoneStatus(prescription)
     prescription.phoneStatus == true ? "有電聯":"未電聯"
   end
 
   # 處方箋取得狀態
   # @param  prescription
   # @return 已取得 or 未取得
-  def obtainStatus prescription
+  def obtainStatus(prescription)
     prescription.obtainStatus == true ? "已取得":"未取得"
   end
 end
