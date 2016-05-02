67990f87db66bddd70521bfbda52eefc24c5ac1b
sheng wei qiu <shadow3x3x3@gmail.com>
2016-05-02 16:13:12 +0800

    using belong to and has many

--- app/models/fit_of_member.rb
+++
@@ -1,4 +0,0 @@
-class FitOfMember < ActiveRecord::Base
-  self.table_name  = 'fit_of_member'
-  self.primary_key = 'fitID'
-end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,11 +2,5 @@
 class Member < ActiveRecord::Base
   self.table_name   = 'member'
   self.primary_key  = 'memberID'
-
-  has_many :fits
-
-  def fits
-    fits_id = FitOfMember.where(memberID: id).pluck(:fitID)
-    Fit.find(fits_id)
-  end
+  has_many :fits, foreign_key: 'memberID'
 end
