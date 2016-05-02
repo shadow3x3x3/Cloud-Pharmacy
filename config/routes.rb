485f9e380005048a29cf716c6e72feb1e195f536
sheng wei qiu <shadow3x3x3@gmail.com>
2016-05-02 15:35:39 +0800

    good smell

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :members, :only => [:index] do
+  resources :members, only: [:index] do
     member do
-      resources :fits, :except => [:show]
+      resources :fits, except: [:show]
     end
   end
-  resources :hospitals, :pharmacists, :except => [:show]
-  resources :agencies, :except => [:show]
-  resources :residents, :except => [:show]
+  resources :hospitals, :pharmacists, except: [:show]
+  resources :agencies, except: [:show]
+  resources :residents, except: [:show]
   resources :assessment_forms, :prescriptions, :drugs
-  root :to => "welcome#index"
+  root to: 'welcome#index'
 
-  get "welcome/say_hello" => "welcome#say"
-  get "welcome" => "welcome#index"
-
-
+  get 'welcome/say_hello' => 'welcome#say'
+  get 'welcome' => 'welcome#index'
 end
