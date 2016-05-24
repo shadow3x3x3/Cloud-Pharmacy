0d59cd5cfb690874bda58983087a43501d518b68
sheng wei qiu <shadow3x3x3@gmail.com>
2016-05-24 16:03:32 +0800

    relations fix

--- app/models/af_pharmacist_assess.rb
+++ app/models/af_pharmacist_assess.rb
@@ -1,6 +1,7 @@
+# AfPharmacistAssess Model
 class AfPharmacistAssess < ActiveRecord::Base
-  self.table_name   = "af_pharmacist_assess"
-  self.primary_key  = "pharmacistAssessID"
+  self.table_name   = 'af_pharmacist_assess'
+  self.primary_key  = 'pharmacistAssessID'
 
   belongs_to :assessment_form
 end
--- app/models/af_prescription_content.rb
+++ app/models/af_prescription_content.rb
@@ -1,7 +1,7 @@
+# AfPrescriptionContent Model
 class AfPrescriptionContent < ActiveRecord::Base
-  self.table_name   = "af_prescription_content"
-  self.primary_key  = "prescriptionContentID"
+  self.table_name   = 'af_prescription_content'
+  self.primary_key  = 'prescriptionContentID'
 
   belongs_to :assessment_form
-
 end
