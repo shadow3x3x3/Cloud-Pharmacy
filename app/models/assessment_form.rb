bd8054cefc01350d0e339ad4fc70330630a0cf33
sheng wei qiu <shadow3x3x3@gmail.com>
2016-05-24 10:02:16 +0800

    refactor

--- app/models/assessment_form.rb
+++ app/models/assessment_form.rb
@@ -1,38 +1,38 @@
+# AssessmentForm Model
 class AssessmentForm < ActiveRecord::Base
-  self.table_name   = "assessment_form"
-  self.primary_key  = "afID"
+  self.table_name   = 'assessment_form'
+  self.primary_key  = 'afID'
 
-  belongs_to :resident, :foreign_key => "residentID"
+  belongs_to :resident, foreign_key: 'residentID'
 
-  has_one :af_prescription_content, :foreign_key => "prescriptionContentID", :dependent => :destroy
-  has_one :af_pharmacist_assess,    :foreign_key => "pharmacistAssessID"   , :dependent => :destroy
-  has_one :af_nurse_handling,       :foreign_key => "nurseHandlingID"      , :dependent => :destroy
+  has_one :af_prescription_content, foreign_key: 'prescriptionContentID',
+                                    dependent: :destroy
+  has_one :af_pharmacist_assess,    foreign_key: 'pharmacistAssessID',
+                                    dependent: :destroy
+  has_one :af_nurse_handling,       foreign_key: 'nurseHandlingID',
+                                    dependent: :destroy
+  has_many :drugs
 
-  accepts_nested_attributes_for :af_prescription_content, :af_pharmacist_assess, :af_nurse_handling,
-                                :allow_destroy => true, :reject_if => :all_blank
+  accepts_nested_attributes_for :af_prescription_content, :af_pharmacist_assess,
+                                :af_nurse_handling,
+                                allow_destroy: true, reject_if: :all_blank
 
   def process
-    who = self.status
-    if who == "nurse"
-      "護士處理中"
-    elsif who == "pharmacist"
-      "藥師處理中"
+    who = status
+    if who == 'nurse'
+      '護士處理中'
+    elsif who == 'pharmacist'
+      '藥師處理中'
     end
   end
 
-  # true => 與目前權限相符
   def process?(current_user_auth)
-    who = self.status
-    if who == current_user_auth # 需要處理
-      return true
-    end
-    return false
+    who = status
+    return true if who == current_user_auth
+    false
   end
 
-  # 回傳住民姓名
-  def residentName
-    Resident.find(self.residentID).name
+  def resident_name
+    Resident.find(residentID).name
   end
-
-
 end
